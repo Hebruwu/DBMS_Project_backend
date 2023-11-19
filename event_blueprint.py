841a491d5f27736a3a93121cf011e7cc686818a4
Daniel Sabanov <daniel@sadogursky.com>
2023-11-19 14:41:05 -0500

    Add Access-Control-Allow-Origin headers to the responses.

--- event_blueprint.py
+++ event_blueprint.py
@@ -1,21 +1,27 @@
 import traceback
-from flask import Blueprint, Response, jsonify, request
 from dataclasses import asdict
-import dateutil.parser, dateutil.tz
 
-from .database.eventhosting import get_engine, get_feedback, insert_event, get_events, insert_invite, update_invite, insert_feedback, get_invites
+import dateutil.parser
+import dateutil.tz
+from flask import Blueprint, Response, jsonify, request
+
+from .database.eventhosting import get_engine, get_feedback, insert_event, get_events, \
+    insert_invite, update_invite, insert_feedback, get_invites
+
+from typing import Tuple
 
 event_routes = Blueprint('event_routes', __name__)
 
+
 @event_routes.route("create", methods=["POST"])
-def create_event() -> Response:
+def create_event() -> Tuple[Response, int]:
     try:
         engine = get_engine()
         create_args: dict = request.json
         create_args["date_from"] = dateutil.parser.parse(create_args["date_from"],
                                                          ignoretz=False)
         create_args["date_to"] = dateutil.parser.parse(create_args["date_to"],
-                                                         ignoretz=False)
+                                                       ignoretz=False)
         insert_event(engine, create_args["name"],
                      create_args["date_from"],
                      create_args["date_to"],
@@ -25,21 +31,31 @@ def create_event() -> Response:
                      create_args["aid"],
                      create_args.get("description"))
     except Exception as e:
-        return jsonify(error=traceback.format_exc()),400
-    return "Success", 200
+        return jsonify(error=traceback.format_exc()), 400
 
-@event_routes.route("events_details", methods = ["GET"])
-def get_events_details() -> Response:
+    response = jsonify("Success")
+    # Don't know if we need to specify CORS header here. Left as is for now.
+    return response, 200
+
+
+@event_routes.route("events_details", methods=["GET"])
+def get_events_details() -> Tuple[Response, int]:
     try:
         engine = get_engine()
         events = get_events(engine)
         events = [asdict(event) for event in events]
+
     except Exception as e:
-        return jsonify(error=traceback.format_exc()),400
-    return jsonify(events)
+        return jsonify(error=traceback.format_exc()), 400
+
+    response = jsonify(events)
+    response.headers[
+        "Access-Control-Allow-Origin"] = "*"  # This is unsafe, but good enough
+    return response, 200
+
 
 @event_routes.route("create/invite", methods=["POST"])
-def create_event_invite() -> Response:
+def create_event_invite() -> Tuple[Response, int]:
     try:
         engine = get_engine()
         create_args = request.json
@@ -48,11 +64,15 @@ def create_event_invite() -> Response:
                       create_args["eid"],
                       create_args["sid"])
     except Exception as e:
-        return jsonify(error=traceback.format_exc()),400
-    return "Success", 200
+        return jsonify(error=traceback.format_exc()), 400
+
+    response = jsonify("Success")
+    # Don't know if we need to specify CORS header here. Left as is for now.
+    return response, 200
+
 
 @event_routes.route("create/feedback", methods=["POST"])
-def create_event_feedback() -> Response:
+def create_event_feedback() -> Tuple[Response, int]:
     try:
         engine = get_engine()
         create_args = request.json
@@ -61,11 +81,15 @@ def create_event_feedback() -> Response:
                         create_args["eid"],
                         create_args["feedback"])
     except Exception as e:
-        return jsonify(error=traceback.format_exc()),400
-    return "Success", 200
+        return jsonify(error=traceback.format_exc()), 400
+
+    response = jsonify("Success")
+    # Don't know if we need to specify CORS header here. Left as is for now.
+    return response, 200
+
 
 @event_routes.route("modify/invite", methods=["PUT"])
-def modify_event_invite() -> Response:
+def modify_event_invite() -> Tuple[Response, int]:
     try:
         engine = get_engine()
         put_args = request.json
@@ -74,43 +98,50 @@ def modify_event_invite() -> Response:
                       put_args["eid"],
                       put_args["attending"])
     except Exception as e:
-        return jsonify(error=traceback.format_exc()),400
-    return "Success", 200
+        return jsonify(error=traceback.format_exc()), 400
 
-@event_routes.route("invites/<username>", methods = ["GET"])
-def get_events_and_invites(username: str) -> Response:
+    response = jsonify("Success")
+    # Don't know if we need to specify CORS header here. Left as is for now.
+    return response, 200
+
+
+@event_routes.route("invites/<username>", methods=["GET"])
+def get_events_and_invites(username: str) -> Tuple[Response, int]:
     try:
         engine = get_engine()
         result = get_invites(engine, username)
         events_and_invites = []
-        for i in range(0,len(result),2):
+        for i in range(0, len(result), 2):
             events_and_invites.append({
                 "invite": asdict(result[i]),
-                "event": asdict(result[i+1])
+                "event": asdict(result[i + 1])
             })
     except Exception as e:
-        return jsonify(error=traceback.format_exc()),400
-    return jsonify(events_and_invites)
+        return jsonify(error=traceback.format_exc()), 400
+    response = jsonify(events_and_invites)
+    response.headers[
+        "Access-Control-Allow-Origin"] = "*"  # This is unsafe, but good enough
+    return response, 200
 
-@event_routes.route("feedback/<eid>", methods = ["GET"])
-def get_students_and_feedback(eid: int) -> Response:
+
+@event_routes.route("feedback/<eid>", methods=["GET"])
+def get_students_and_feedback(eid: int) -> Tuple[Response, int]:
     try:
         engine = get_engine()
         result = get_feedback(engine, eid)
         students_and_feedback = []
-        for i in range(0,len(result),2):
-            student = asdict(result[i+1])
+        for i in range(0, len(result), 2):
+            student = asdict(result[i + 1])
             del student["PASSWORD"]
             students_and_feedback.append({
                 "feedback": asdict(result[i]),
                 "student": student
             })
     except Exception as e:
-        return jsonify(error=traceback.format_exc()),400
-    return jsonify(students_and_feedback)
+        return jsonify(error=traceback.format_exc()), 400
 
-    
-        
-        
+    response = jsonify(students_and_feedback)
+    response.headers[
+        "Access-Control-Allow-Origin"] = "*"  # This is unsafe, but good enough
 
-
+    return response, 200
--- student_blueprint.py
+++ student_blueprint.py
@@ -1,54 +1,73 @@
 import traceback
 from flask import Blueprint, Response, jsonify, request
 from dataclasses import asdict
-from .database.eventhosting import get_engine, get_student, authenticate_student, insert_student, get_students
+from .database.eventhosting import get_engine, get_student, authenticate_student, \
+    insert_student, get_students
+
+from typing import Tuple
 
 student_routes = Blueprint('student_routes', __name__)
 
-@student_routes.route("/student_details/<username>", methods = ["GET"])
-def get_student_details(username: str) -> Response:
+
+@student_routes.route("/student_details/<username>", methods=["GET"])
+def get_student_details(username: str) -> Tuple[Response, int]:
     try:
         engine = get_engine()
         student = get_student(engine, username)
         student = asdict(student)
         del student["PASSWORD"]
-        return student
-    except Exception as e:
-        return jsonify(error=traceback.format_exc()),500
+        response = jsonify(student)
+        response.headers[
+            "Access-Control-Allow-Origin"] = "*"  # This is unsafe, but good enough
 
-@student_routes.route("/students_details", methods = ["POST"])
-def get_students_details() -> Response:
+        return response, 200
+
+    except Exception as e:
+        return jsonify(error=traceback.format_exc()), 500
+
+
+@student_routes.route("/students_details", methods=["POST"])
+def get_students_details() -> Tuple[Response, int]:
     try:
         engine = get_engine()
         get_args: dict = request.json
         result = get_students(engine,
-                     get_args.get("majors", ["%"]),
-                     get_args.get("citizenships", ["%"]),
-                     get_args.get("races", ["%"]),
-                     get_args.get("genders", ["%"]))
+                              get_args.get("majors", ["%"]),
+                              get_args.get("citizenships", ["%"]),
+                              get_args.get("races", ["%"]),
+                              get_args.get("genders", ["%"]))
         students = []
         for student in result:
             students.append(asdict(student))
             del students[-1]["PASSWORD"]
     except Exception as e:
-        return jsonify(error=traceback.format_exc()),400
-    return jsonify(students)
-    
-        
-    
+        return jsonify(error=traceback.format_exc()), 400
+
+    response = jsonify(students)
+    response.headers[
+        "Access-Control-Allow-Origin"] = "*"  # This is unsafe, but good enough
+    return response, 200
+
+
 @student_routes.route("authenticate", methods=["POST"])
-def authenitcate() -> Response:
+def authenticate() -> Tuple[Response, int]:
     try:
         engine = get_engine()
         auth_args = request.json
-        authenitcated = authenticate_student(engine, auth_args["username"], auth_args["password"])
-        return jsonify(is_authenticated=authenitcated), (200 if authenitcated \
-        else 401)
+        authenticated = authenticate_student(engine, auth_args["username"],
+                                             auth_args["password"])
+        response = jsonify(is_authenticated=authenticated)
+        response.headers[
+            "Access-Control-Allow-Origin"] = "*"  # This is unsafe, but good enough
+
+        return response, (200 if authenticated else 401)
+
     except Exception as e:
-        return jsonify(error=traceback.format_exc()),500
+        return jsonify(error=traceback.format_exc()), 500
+
 
 @student_routes.route("create", methods=["POST"])
-def create_student() -> Response:
+def create_student() -> Tuple[Response, int]:
     try:
         create_args: dict = request.json
         engine = get_engine()
@@ -64,6 +83,6 @@ def create_student() -> Response:
                        create_args.get("year"),
                        create_args["gender"])
     except Exception as e:
-        return jsonify(error=traceback.format_exc()),400
-    return "Success", 200
-    
+        return jsonify(error=traceback.format_exc()), 400
+
+    return jsonify("Success"), 200
