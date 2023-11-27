484d0559e6bfe7eb3bc8586474988b340e6f10ea
Sparsh Joshi <sparshjoshi1998@vt.edu>
2023-11-27 16:06:11 -0500

    Enable Cors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, Blueprint
+from flask_cors import CORS
 from .student_blueprint import student_routes
 from .admin_blueprint import admin_routes
 from .event_blueprint import event_routes
@@ -7,6 +8,7 @@ app = Flask(__name__)
 app.register_blueprint(student_routes, url_prefix="/eventhosting/students")
 app.register_blueprint(admin_routes, url_prefix="/eventhosting/admins")
 app.register_blueprint(event_routes, url_prefix="/eventhosting/events")
+CORS(app)
  
 @app.route('/')
 def hello_world():
